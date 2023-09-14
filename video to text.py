dcb26cc1001560be4f9d881ca4bab6d00173b652
AstroBesat <128177174+AstroBesat-SoftW@users.noreply.github.com>
2023-09-14 13:21:13 +0300

    Add files via upload

---
+++ video to text.py
@@ -0,0 +1,22 @@
+import moviepy.editor as mp
+import speech_recognition as sr
+
+# MP4 dosyasını yükle
+video_file = "20230524_034658.mp4"
+
+# MP4 dosyasından sesi çıkart
+clip = mp.VideoFileClip(video_file)
+clip.audio.write_audiofile("temp.wav")
+
+# Ses dosyasını tanıma işlemini yap
+audio_file = "temp.wav"
+recognizer = sr.Recognizer()
+
+with sr.AudioFile(audio_file) as source:
+    audio = recognizer.record(source)
+    try:
+        # Ses dosyasını metne çevir
+        text = recognizer.recognize_google(audio, language="tr-TR")
+        print(text)
+    except sr.UnknownValueError:
+        print("Ses tanınmadı.")
